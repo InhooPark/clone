8bf5ac98aadd4ca9234d4526f2bab67d3073074d
InhooPark <48538800+InhooPark@users.noreply.github.com>
2022-12-02 17:35:23 +0900

    221202
    11

--- firstproject/js/common.js
+++ firstproject/js/common.js
@@ -1,3 +1,4 @@
+// 업버튼 fixed, scale 애니메이션
 const scTopbtn = document.querySelector('.upbtnside');
 const DISABLE = 'disable'
 
@@ -23,3 +24,29 @@ window.addEventListener('scroll', function() {
 
 
 
+// 업버튼 스무스하게 위로 올라가기
+document.querySelector(".upbtn").addEventListener("mouseup", () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
+});
+
+// 뉴스레터 판정
+const mailBtn = document.getElementById('emailBtn');
+const mailCheckText = document.querySelector('.email-check-text')
+
+mailBtn.addEventListener('mouseup', function() {
+    const mailCheck = document.getElementById('email').value;
+    check = /\S+@+\S+.+\S/;
+    if(check.test(mailCheck)){
+        mailCheckText.textContent = `입력하신 이메일 주소(${mailCheck})로 확인 메일을 보내드렸습니다.`;
+    }
+    else if(mailCheck == ''){
+        mailCheckText.textContent = `이메일 주소를 입력하세요.`;
+    }
+    else{
+        mailCheckText.textContent = `잘못된 이메일 주소입니다.`;
+    }
+})
--- firstproject/js/home.js
+++ firstproject/js/home.js
@@ -1,3 +1,4 @@
+// 공지 로테이션
 const SHOWING_CLASS = "showing";
 
 const firstSlide = document.querySelector(".slide:first-child");
@@ -17,6 +18,7 @@ function slide() {
     }
 }
 
+// 뉴스 좌우클릭 제한이 안걸려있음 LOOP도 없고 구림 다음엔 제대로
 slide();
 setInterval(slide, 3000);
 
@@ -27,14 +29,23 @@ let NEWSWRAP = document.querySelector(".news-content-wrap");
 
 let leftcount = 0;
 let rightcount = 0;
+let moveP;
 
 LEFTBTN.onclick = function () {
+    moveP = document.querySelector('.news-content').offsetWidth;
+    console.log(moveP);
+    // 마진값을 더해줘도 왜 차이가 나는걸까 나중에
     leftcount++;
     rightcount--;
-    NEWSWRAP.style = `transform: translateX(${leftcount * -35}%)`;
+    NEWSWRAP.style = `transform: translateX(${-(leftcount * moveP + 50)}px)`;
 };
 RIGHTTBTN.onclick = function () {
+    moveP = document.querySelector('.news-content').offsetWidth;
+    console.log(moveP);
     leftcount--;
     rightcount++;
-    NEWSWRAP.style = `transform: translateX(${rightcount * 35}%)`;
+    NEWSWRAP.style = `transform: translateX(${(rightcount * moveP + 50)}px)`;
 };
+
+// 스크롤시 텍스트 애니메이션
+// parallax.js 써보기

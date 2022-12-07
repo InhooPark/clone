fa869d16b803dad6d872fc9024533700c0a87da3
InhooPark <48538800+InhooPark@users.noreply.github.com>
2022-12-07 11:19:00 +0900

    221207
    open api

---
+++ js/contact.js
@@ -0,0 +1,9 @@
+var mapOptions = {
+    center: new naver.maps.LatLng(37.3595704, 127.105399),
+    zoom: 10
+};
+
+var map = new naver.maps.Map('map', {
+    center: new naver.maps.LatLng(37.3595704, 127.105399),
+    zoom: 10
+});
